625695373405b2145e811dfe7dc731f497d68a2a
dronesito <dronesitodron@gmail.com>
2025-04-18 11:24:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -13,13 +13,39 @@ document.querySelectorAll('.block2').forEach(block => {
     const originalText = title.textContent;
     const hoverContent = block.querySelector('.hover-content');
     const hoverTitleText = hoverContent.querySelector('.hover-title-text');
-    block.addEventListener('mouseenter', () => {
+    // Проверяем ширину экрана
+    if (window.innerWidth >= 768) { // Если экран больше или равен 768px
+        block.addEventListener('mouseenter', () => {
+            hoverTitleText.textContent = title.getAttribute('data-hover-title');
+            hoverContent.style.display = 'block'; // Показываем новый контент
+            title.style.display = 'none'; // Скрываем оригинальный заголовок
+        });
+        block.addEventListener('mouseleave', () => {
+            hoverContent.style.display = 'none'; // Скрываем новый контент
+            title.style.display = 'block'; // Показываем оригинальный заголовок
+        });
+    } else {
+        // Если экран меньше 768px, сразу показываем контент
         hoverTitleText.textContent = title.getAttribute('data-hover-title');
         hoverContent.style.display = 'block'; // Показываем новый контент
         title.style.display = 'none'; // Скрываем оригинальный заголовок
-    });
-    block.addEventListener('mouseleave', () => {
-        hoverContent.style.display = 'none'; // Скрываем новый контент
-        title.style.display = 'block'; // Показываем оригинальный заголовок
-    });
+    }
+});
+
+
+
+
+const menuToggle = document.querySelector('.menu-toggle');
+const mobileMenu = document.querySelector('.mobile-menu');
+// Скрываем иконку закрытия по умолчанию
+const closeIcon = 'img/Property 1=Variant2.png'; // Иконка закрытия
+const openIcon = 'img/Component 5.png'; // Иконка открытия
+menuToggle.addEventListener('click', function() {
+    mobileMenu.classList.toggle('active'); // Добавляем или убираем класс active
+    // Меняем иконки
+    if (mobileMenu.classList.contains('active')) {
+        menuToggle.src = closeIcon; // Иконка закрытия
+    } else {
+        menuToggle.src = openIcon; // Иконка открытия
+    }
 });
